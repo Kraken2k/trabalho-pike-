da57f8b30f0ccfefe291a230198b53e6e7950127
Matheus Oliveira <mtz@MacBook-Pro-de-Matheus.local>
2025-04-21 18:48:08 -0300

    teste bd macbook

--- providers/ClerkAndConvexProvider.tsx
+++ providers/ClerkAndConvexProvider.tsx
@@ -1,11 +1,11 @@
 import { ClerkLoaded, ClerkProvider, useAuth } from "@clerk/clerk-expo";
 import { tokenCache } from "cache";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
-import { ConvexReactClient }from "convex/react";
+import { ConvexReactClient } from "convex/react";
 
-const convex = new ConvexReactClient{process.env.EXPO_PUBLIC_CONVEX_URL!, {
+const convex = new ConvexReactClient (process.env.EXPO_PUBLIC_CONVEX_URL!, {
     unsavedChangesWarning: false,
-}};
+});
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
